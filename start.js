10b912ba43c9dcfa824f41aa86efe350ae91f974
Vaidas Pilkauskas <vaidas.pilkauskas@gmail.com>
2016-06-29 22:47:53 +0300

    Add initial project structure

---
+++ start.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express'),
+  bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+
+app.get('/hello', (req, res) => {
+  res.send('hello');
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Listening...');
+});
