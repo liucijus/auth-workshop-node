25bcffd6d4f0b49fc26740fce4b5940d5a476bd4
Vaidas Pilkauskas <vaidas.pilkauskas@gmail.com>
2016-07-26 22:26:39 +0300

    First day

--- start.js
+++ start.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const express = require('express'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  cookieParser = require('cookie'),
+  crypto = require('crypto-js');
 
 const app = express();
 
@@ -10,8 +12,38 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
+const SECRET = '<change me>';
+
+function hashOf(value) {
+  return crypto.HmacSHA256(value.toString(), SECRET).toString();
+}
+
+app.get('/visitors', (req, res) => {
+  const cookieHeaderValue = req.header('cookie') || '';
+  const visitCount = cookieParser.parse(cookieHeaderValue).visitCount || '';
+
+  const [value, hash] = visitCount.split('|');
+
+  /* Node 4
+   const cookieParts = visitCount.split('|');
+
+   const value = cookieParts[0];
+   const hash = cookieParts[1];
+   */
+
+  const count = (!isNaN(value) && hashOf(value) === hash) ? parseInt(value) + 1 : 1;
+
+  const newHash = hashOf(count);
+
+  const cookieValue = count.toString() + '|' + newHash;
+
+  res.cookie('visitCount', cookieValue);
+  res.render('visits', {visitCount: count});
+});
+
+
 app.get('/hello', (req, res) => {
-  res.send('hello');
+  res.send('<h1>foobar</h1>');
 });
 
 app.get('/', (req, res) => {
